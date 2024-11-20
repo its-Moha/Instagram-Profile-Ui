e3da3ece251e80b77617cf239700fab8f879c398
mohamedamin <funstars008@gmail.com>
2024-11-20 13:41:13 +0300

    adding our Instagram Profile Ui to a profile screen file and clean up some codes and prepare the development environment

--- app/src/main/java/com/example/instagramprofileui/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofileui/MainActivity.kt
@@ -16,32 +16,9 @@ import com.example.instagramprofileui.ui.theme.InstagramProfileUiTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            InstagramProfileUiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ProfileScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstagramProfileUiTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/instagramprofileui/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.example.instagramprofileui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(){
+    Column(modifier = Modifier.fillMaxSize()){  }
+}
